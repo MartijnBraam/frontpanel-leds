7527e09c251d2511e8a666236167c3a088275d32
Martijn Braam <martijn@brixit.nl>
2016-02-19 11:41:15 +0100

    Fixed network bars

--- driver/leds.py
+++ driver/leds.py
@@ -6,6 +6,7 @@ import psutil
 import argparse
 
 interval = 0.5
+netmax = 10 * 1024
 
 
 def write_packet(packet):
@@ -31,11 +32,16 @@ def write_column(column):
     write_packet(column)
 
 
-def render_graph_point(value, min=0, max=100):
+def scale_to_leds(value, min, max):
     value = (value - min) / max
-    val = int((value * 6) + 0.5)
-    if val > 5:
-        val = 5
+    leds = int((value * 6) + 0.5)
+    if leds > 5:
+        leds = 5
+    return leds
+
+
+def render_graph_point(value, min=0, max=100):
+    val = scale_to_leds(value, min, max)
 
     val = 5 - val
 
@@ -46,6 +52,17 @@ def render_graph_point(value, min=0, max=100):
     row[val] = 1
 
     write_column(row)
+
+
+def render_bar(frame, col, value, min=0, max=100):
+    dots = scale_to_leds(value, min, max)
+    for i in range(0, dots):
+        frame[col][5 - i] = 1
+
+    if dots > 0:
+        frame[col][6 - dots] = 1
+
+    return frame
 
 
 def render_system_usage_bars(cpu, mem, netin, netout):
@@ -61,31 +78,17 @@ def render_system_usage_bars(cpu, mem, netin, netout):
 
     frame = render_bar(frame, 0, cpu)
     frame = render_bar(frame, 2, mem)
-    frame = render_bar(frame, 4, netin)
-    frame = render_bar(frame, 6, netout)
-    return frame
-
-
-def render_bar(frame, col, value):
-    dots = int((value * 6) + 0.5)
-    if dots > 5:
-        dots = 5
-
-    for i in range(0, dots):
-        frame[col][5 - i] = 1
-
-    if dots > 0:
-        frame[col][6 - dots] = 1
-
+    frame = render_bar(frame, 4, netin, max=netmax)
+    frame = render_bar(frame, 6, netout, max=netmax)
     return frame
 
 
 def mode_bars():
     while True:
-        cpu = psutil.cpu_percent() / 100.0
-        mem = psutil.virtual_memory().percent / 100.0
-        netin = 0.5
-        netout = 0.5
+        cpu = psutil.cpu_percent()
+        mem = psutil.virtual_memory().percent
+        netin = psutil.net_io_counters(pernic=False).bytes_recv / 1024
+        netout = psutil.net_io_counters(pernic=False).bytes_sent / 1024
         frame = render_system_usage_bars(cpu, mem, netin, netout)
         write_frame(frame)
         sleep(0.3)
@@ -144,8 +147,12 @@ if __name__ == "__main__":
     parser.add_argument('--hflip', help="Flip the display output on the horisontal axis", action="store_true")
     parser.add_argument('--vflip', help="Flip the display output on the vertical axis", action="store_true")
     parser.add_argument('--interval', '-i', help="Update interval for chart modes in seconds", type=float, default=0.5)
+    parser.add_argument('--netmax', '-n',
+                        help="The scaling for network usage. Defines the maximum value in KB/s (default 10 MB/s)",
+                        default=10 * 1024, type=int)
     parser.add_argument('device', help="Path to the serial port", default="/dev/frontpanel")
     parser.add_argument('mode', help="Chooses what to display", choices=['bars', 'graph-cpu', 'graph-memory', 'demo'])
     args = parser.parse_args()
     interval = args.interval
+    netmax = args.netmax
     main(args.device, args.mode, args.hflip, args.vflip)
